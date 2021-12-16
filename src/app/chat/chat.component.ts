fdeef8d9c41f44af760035e0f9b2921edb175215
Costigan-Stephen <stephenjcostigan@hotmail.com>
2021-12-15 21:59:11 -0600

    convos done

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Chat } from './chat.model';
+import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  currentSender = '101';
+
+  @ViewChild('messageText', { static: true }) messageText: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Chat>();
+  
+
+  constructor(private chatService: ChatService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSendMessage() {
+    if(this.messageText.nativeElement.value){
+      const id = new Date().getTime().toString();
+      var str = this.router.url;
+      const convoid = str.replace(/\D/g, "");
+      const newMessage = new Chat(
+        id, 
+        "chatwombat", 
+        this.messageText.nativeElement.value, 
+        this.currentSender,
+        convoid
+      );
+      this.chatService.addMessage(newMessage);
+      this.onClear();
+    }
+  }
+
+  onClear() {
+    this.messageText.nativeElement.value = '';
   }
 
 }
